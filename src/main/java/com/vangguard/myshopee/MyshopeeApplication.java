d5f12f5ac09490cf1c34d10ace73bcd5527c94bc
abhijeet <abhijeet.thakur1@gmail.com>
2023-05-18 19:11:11 +0530

    first commit

---
+++ src/main/java/com/vangguard/myshopee/MyshopeeApplication.java
@@ -0,0 +1,13 @@
+package com.vangguard.myshopee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshopeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshopeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vangguard/myshopee/MyshopeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vangguard.myshopee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshopeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
